8070ea29064cbc2b0a3c9d2590d38857e6daad01
Gleb Kiselev <12bny23gleb@gmail.com>
2019-07-29 17:30:38 +0300

    Changed the data for authorization in the database

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,20 +1,20 @@
 const pg = require('pg');
 
-const config = {
-  user: 'postgres',
-  database: 'post',
-  password: 'barneo24', 
-  host: 'localhost', 
-  port: '5432'
-};
-
 // const config = {
-//   user: 'dzyndakohodrhg',
-//   database: 'ddh62e3vm69ef0',
-//   password: '3386c08c0f1977f7d9c6651658ca55d09918059d6a675b4164acfc2619e6beec',
-//   host: 'ec2-54-246-84-100.eu-west-1.compute.amazonaws.com', 
+//   user: 'postgres',
+//   database: 'post',
+//   password: 'barneo24', 
+//   host: 'localhost', 
 //   port: '5432'
 // };
+
+const config = {
+  user: 'dzyndakohodrhg',
+  database: 'ddh62e3vm69ef0',
+  password: '3386c08c0f1977f7d9c6651658ca55d09918059d6a675b4164acfc2619e6beec',
+  host: 'ec2-54-246-84-100.eu-west-1.compute.amazonaws.com', 
+  port: '5432'
+};
 
 const pool = new pg.Pool(config);
 
